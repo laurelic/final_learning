59bd45d381c0b926538592f919ec54f4d58fe159
laurelic <37823073+laurelic@users.noreply.github.com>
2018-11-27 18:03:02 -0800

    added table generator

--- app.py
+++ app.py
@@ -1,36 +1,30 @@
-#!/usr/bin/env python3
 import sys
 from flask import Flask, render_template,jsonify,request
 from flask_cors import CORS
-# from flask_sqlalchemy import SQLAlchemy
-# from sqlalchemy.ext.automap import automap_base
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import Session
-# import numpy as np
-# import pandas as pd
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+import numpy as np
+import pandas as pd
 
 app = Flask(__name__)
 CORS(app)
 
-# app.config['STATIC_FOLDER'] = 'static'
-# app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database/inpatient.db"
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# db = SQLAlchemy(app)
+#app.config['STATIC_FOLDER'] = 'static'
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db/inpatient.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-# Base = automap_base(metadata=db.metadata)
-# engine = db.get_engine()
-# Base.prepare(engine, reflect=True)
-# Inpatient = Base.classes.inpatient
-# Drg = Base.classes.drg
-
-
-#@app.route("/map")
-#def map():
-
-#    return render_template("map.html",xpage="map")
+Base = automap_base(metadata=db.metadata)
+engine = db.get_engine()
+Base.prepare(engine, reflect=True)
+Inpatient = Base.classes.inpatient
+Drg = Base.classes.drg
 
 @app.route("/")
 def index():
+    r"""Display the intro page"""
     return render_template("index.html")
 
 @app.route("/explore")
@@ -43,9 +37,21 @@ def predict():
 
 @app.route("/data")
 def data():
-    """Display the data table"""
+    r"""Display the data page"""
     #res = db.session.query(Inpatient).all()
     return render_template("data.html")
+
+@app.route("/inpatient_data")
+def inpatient_data():
+    r""" Returns a json of the inpatient data"""
+
+    response = db.session.query(Inpatient).all()
+    d_list = []
+    for r in response:
+        rec = r.__dict__.copy()
+        d_list.append(rec)
+    return jsonify(d_list)
+        
 
 	
 # @app.route("/data")
@@ -104,62 +110,5 @@ def data():
 #     print(sample_metadata)
 #     return jsonify(sample_metadata)   
 	
-	
-#@app.route("/superfund_sites/<idname>")
-#def sample_metadata(idname):
-#    """Return the metadata for a given superfund site name."""
-#    sel = [
-#        Superfund.id,
-#        Superfund.epa_id,
-#        Superfund.site_id,
-#        Superfund.name,
-#        Superfund.address,
-#        Superfund.city,
-#        Superfund.state,
-#        Superfund.xzip,
-#        Superfund.latitude,
-#        Superfund.longitude,
-#        Superfund.hrs_score,
-#        Superfund.idname,
-#    ]
-
-#   results = db.session.query(*sel).filter(Superfund.idname == idname).all()
-
-    # Create a dictionary entry for each row of metadata information
-#    sample_metadata = {}
-#    for result in results:
-#        sample_metadata["id"] = result[0]
-#        sample_metadata["epa_id"] = result[1]
-#        sample_metadata["site_id"] = result[2]
-#        sample_metadata["name"] = result[3]
-#        sample_metadata["address"] = result[4]
-#        sample_metadata["city"] = result[5]
-#        sample_metadata["state"] = result[6]
-#        sample_metadata["xzip"] = result[7]
-#        sample_metadata["latitude"] = result[8]
-#        sample_metadata["longitude"] = result[9]
-#        sample_metadata["hrs_score"] = result[10]
-#        sample_metadata["idname"] = result[11]
-
-#    print(sample_metadata)
-#    return jsonify(sample_metadata)
-
-#@app.route("/life_expectancy")
-#def life_expectancy():
-#    r""" This function returns the average life expectancy
-#    by zip code and geo coordinates"""
-
-#    res = db.session.query(LifeExpectancy).all()
-
-#    dlist = []
-#    for dset in res:
-#        md = dset.__dict__.copy()
-#        del md['_sa_instance_state']
-#        dlist.append(md)
-        
-#    return jsonify(dlist)
-    
-
-
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ static/js/create_table.js
@@ -0,0 +1,21 @@
+var table = new Tabulator("#inpatient-table", {
+    pagination: "local",
+    paginationSize: 50,
+    layout: "fitColumns",
+    columns:[
+        {title: "DRG Name", field: "drg_definition"},
+        {title: "Prodiver ID", field: "provider_id"},
+        {title: "Provider Name", field: "provider_name"},
+        {title: "Provider Address", field: "provider_street_address"},
+        {title: "Provider City", field: "provider_city"},
+        {title: "Provider State", field: "provider_state"},
+        {title: "Provider Zip", field: "provider_zip_code"},
+        {title: "HRR Description", field: "hrr_description"},
+        {title: "Total Discharges", field: "total_disch"},
+        {title: "Avg Covered Charges", field: "average_covered_charges"}
+    ],
+});
+
+d3.json("/inpatient_data", function(data) {
+    table.setData(data);
+});
