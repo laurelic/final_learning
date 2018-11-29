4fcb3e4a4ae628e62642bf3c7143b7ca9512ca10
laurelic <37823073+laurelic@users.noreply.github.com>
2018-11-29 12:48:44 -0800

    added loading spinner to content

--- static/js/create_table.js
+++ static/js/create_table.js
@@ -1,22 +1,26 @@
 var table = new Tabulator("#inpatient-table", {
     pagination: "local",
-    paginationSize: 50,
+    paginationSize: 15,
     layout: "fitColumns",
     columns:[
         {title: "DRG Name", field: "drg_definition"},
         {title: "Prodiver ID", field: "provider_id"},
-        {title: "Provider Name", field: "provider_name"},
+        {title: "Provider Name", field: "provider_name", headerFilter:true},
         {title: "Provider Address", field: "provider_street_address"},
-        {title: "Provider City", field: "provider_city"},
-        {title: "Provider State", field: "provider_state"},
-        {title: "Provider Zip", field: "provider_zip_code"},
-        {title: "HRR Description", field: "hrr_description"},
-        {title: "Total Discharges", field: "total_disch"},
-        {title: "Avg Covered Charges", field: "average_covered_charges"}
+        {title: "Provider City", field: "provider_city", headerFilter:true},
+        {title: "Provider State", field: "provider_state", headerFilter:true},
+        {title: "Provider Zip", field: "provider_zip_code", headerFilter:true},
+        {title: "HRR Description", field: "hrr_description", headerFilter:true},
+        {title: "Total Discharges", field: "total_discharges"},
+        {title: "Avg Covered Charges", field: "average_covered_charges"},
+        {title: "Avg Total Payments", field: "average_total_payments"}
     ],
 });
 
+$("#tabulator-controls button[name=download").on("click", function(){
+    table.download("csv", "InpatientMedicareByHRR.csv")
+})
+
 d3.json("/inpatient_data", function(data) {
-    console.log(data)
     table.setData(data);
 });
